77f91cffc3ec8934a866b0180a6c84d9208c408a
eeu825 <34313713+eeu825@users.noreply.github.com>
2017-12-06 17:45:26 +0000

    Update Optician.java

--- Optician.java
+++ Optician.java
@@ -1,3 +1,55 @@
-//TODO
+
+import java.sql.*;
+
+/**
+* Optician
+*
+*/
+
 public class Optician {
+  
+  private int ID;
+  private String opticianName;
+  private String address;
+  private String postcode;
+  private String phoneNumber;
+  private Boolean nhsORprivate;
+  
+  public Optician(int ID, String opticianName, String address, String postcode, String phoneNumber, Boolean nhsORprivate) {
+    this.ID = ID;
+    this.opticianName = opticanName;
+    this.address = address;
+    this.postcode = postcode;
+    this.phoneNumber = phoneNumber;
+    this.nhsORprivate = nhsORprivate;
+}
+  
+  public int getID() {
+    return ID;
+  }
+  
+  public void setID(int ID) {
+    this.ID = ID;
+  }
+  
+  public String opticanName() {
+    return opticianName;
+  }
+  
+  public void setAddress(String address) {
+    this.address = address;
+  }
+  
+  public String getPostcode() {
+    return postcode;
+  }
+  
+  public String getPhoneNumber() {
+    return phoneNumber;
+  }
+  
+  public void setnhsORprivate(Boolean nhsORprivate) {
+    this.nhsORprivate = nhsORprivate;
+  }
+  
 }
